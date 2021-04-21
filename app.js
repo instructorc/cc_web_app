d9d177885b498a46b6a502b6b274849fbc4b88b2
C. Fulton <cfulton@duwnoody.edu>
2021-04-21 07:37:19 -0500

    adding comment to app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Bring in routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var submitRouter = require('./routes/submit');
